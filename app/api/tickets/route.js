3f31165fe09a4a30c1ebba4553742d833647e125
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-12 09:52:46 +0000

    learning handling get routes 18

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,1 +1,9 @@
-// endpoint = /api/tickets
+import { NextResponse } from "next/server"; // https://nextjs.org/docs/app/api-reference/functions/next-response
+import { url } from "@/app/app.config.js";
+
+export async function GET() {
+  console.log(url);
+  console.log
+  let tickets = await fetch(url+"/tickets").then(r => r.json());
+  return new NextResponse.json(tickets, { status: 200 });
+}
