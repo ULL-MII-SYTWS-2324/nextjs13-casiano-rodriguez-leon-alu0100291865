bb23becc63dd162ecb3db709e7d4f399f9199385
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-12 12:00:12 +0000

    merged with learn-20

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,29 +1,26 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server"; // https://nextjs.org/docs/app/api-reference/functions/next-response
+import { url } from "@/app/app.config.js";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic' // https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#opting-out-of-data-caching
 
 export async function GET() {
-  const res = await fetch('http://localhost:4000/tickets')
+    console.log(url);
 
-  const tickets = await res.json()
-
-  return NextResponse.json(tickets, {
-    status: 200
-  })
+    let tickets = await fetch(url + "/tickets").then(r => r.json());
+    return NextResponse.json(tickets, { status: 200 }); // https://nextjs.org/docs/app/api-reference/functions/next-response#json
 }
 
+/* 
+You can use the next.revalidate option of fetch to set the revalidation period (in seconds) 
+of an individual fetch request
+*/
+
 export async function POST(request) {
-  const ticket = await request.json()
-
-  const res = await fetch('http://localhost:4000/tickets', {
-    method: 'POST',
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(ticket)
-  })
-
-  const newTicket = await res.json()
-
-  return NextResponse.json(newTicket, {
-    status: 201
-  })
+    let body = await request.json(); // request.json() see https://nextjs.org/docs/app/building-your-application/routing/route-handlers#request-body
+    let ticket = await fetch(url + "/tickets", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body) 
+    }).then(r => r.json());
+    return NextResponse.json(ticket, { status: 201 }); // 201: a new resource was created
 }
