7a4d2f73380c95721f6ca2294630119dd6d8d5cc
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-12 10:34:39 +0000

    learning dynamic versus static route handlers 19

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,13 +1,12 @@
 import { NextResponse } from "next/server"; // https://nextjs.org/docs/app/api-reference/functions/next-response
 import { url } from "@/app/app.config.js";
 
+export const dynamic = 'force-dynamic' 
+
 export async function GET() {
   console.log(url);
   
-  let tickets = await fetch(url+"/tickets", { 
-    cache: "no-cache", // Also `cache: 'no-store'`  https://nextjs.org/docs/app/building-your-application/caching#fetch-optionscache
-    next: { revalidate: 3600 } // https://nextjs.org/docs/app/building-your-application/caching#fetch-optionsnextrevalidate
-  }).then(r => r.json());
+  let tickets = await fetch(url+"/tickets").then(r => r.json());
   return NextResponse.json(tickets, { status: 200 }); // https://nextjs.org/docs/app/api-reference/functions/next-response#json
 }
 
