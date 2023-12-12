cc6cc69f7a0027b19660c09210a3f5ed182ddb91
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-12 10:12:30 +0000

    learning handling get routes 18

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -10,3 +10,8 @@ export async function GET() {
   }).then(r => r.json());
   return NextResponse.json(tickets, { status: 200 }); // https://nextjs.org/docs/app/api-reference/functions/next-response#json
 }
+
+/* 
+You can use the next.revalidate option of fetch to set the revalidation period (in seconds) 
+of an individual fetch request
+*/
